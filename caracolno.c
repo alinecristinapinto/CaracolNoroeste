3135d35cd7472c1ea397772a9ac51d39b99a33c3
Aline Cristina Pinto <alinecristina3.14nto@gmail.com>
2022-04-28 19:45:48 -0300

    Solves caracol noroeste problem

--- caracolno.c
+++ caracolno.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 
-void main(){
-    int n, coordenadaX = 0, coordenadaY = 0;
-    scanf("%d", &n);
+typedef struct 
+{
+    int x;
+    int y;
+} Ponto;
 
-    printf("( %d, %d )\n", coordenadaX, coordenadaY);
+void main(){
+    int n;
+    Ponto ponto = { 0, 0 };
+    
+    scanf("%d", &n);
+    
+    if(n%2 == 0){ // N eh par
+        if(n%4==0){ // Verifica se N eh multiplo de 4
+            ponto.x = n/2;
+        } else {
+            ponto.x = -(n + 2)/2;
+        }
+    } else { // N eh impar
+        ponto.y = (n + 1)/2;
+        
+        if((n+1)%4!=0){ // O sucessor de N nao eh multiplo de 4
+            ponto.x = -1;
+        }
+    }
+
+    printf("( %d, %d )\n", ponto.x, ponto.y);
 }
